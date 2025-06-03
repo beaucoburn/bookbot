e253bd951250e77224f2f06d63da7dfd74dfa498
beaucoburn <douglas.coburn@gmail.com>
2025-06-03 11:26:24 -0600

    function can read file path

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-file_path = input("What is the path to the book you want to read?: ")
 def get_book_text(file_path):
-    with open(file_path) as f:
+    with open(file_path, 'r') as f:
         book_text = f.read()
         return book_text
 
-def main():
+def main(file_path):
     book_text = get_book_text(file_path)
     print(book_text)
 
-main()
+main("books/frankenstein.txt")
