bcb4c1f509512c1465609fec3370c7c38e74df74
beaucoburn <douglas.coburn@gmail.com>
2025-06-03 11:06:52 -0600

    add main function

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ def get_book_text(file_path):
     with open(file_path) as f:
         book_text = f.read()
         return book_text
+
+def main():
+    book_text = get_book_text(file_path)
+    print(book_text)
