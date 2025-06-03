cc0ff354fb09cfabdb89f19d44ad4842706de121
beaucoburn <douglas.coburn@gmail.com>
2025-06-03 11:05:02 -0600

    get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("greetings boots")
+file_path = input("books/frankenstein.txt")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        book_text = f.read()
+        return book_text
