1c27de4ff6be224ecb116652af3e083d63643a50
beaucoburn <douglas.coburn@gmail.com>
2025-06-04 13:46:18 -0600

    Update stats.py with count_char function

--- stats.py
+++ stats.py
@@ -1,4 +1,13 @@
 def get_num_words(book_text):
     return len(book_text.split())
 
+def count_char(book_text):
+    dict = {}
+    text = book_text.lower()
+    for t in text:
+        if t not in dict:
+            dict[t] = 1
+        else:
+            dict[t] += 1
+    return dict
 
