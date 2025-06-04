2f62af8855a135114821f0c0d5a0c42c6ed20658
beaucoburn <douglas.coburn@gmail.com>
2025-06-04 07:04:39 -0600

    Added a function to count the number of words in a book

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ def get_book_text(file_path):
         book_text = f.read()
         return book_text
 
+num_words = 0
+def word_count(book_text):
+    num_words = book_text.split()
+    return num_words
+
+
 def main(file_path):
     book_text = get_book_text(file_path)
     print(book_text)
