2dc4ffcd7b7b5dcec27247553463653c7edd29aa
beaucoburn <douglas.coburn@gmail.com>
2025-06-04 07:16:09 -0600

    delete word count function and add to main

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ def get_book_text(file_path):
         book_text = f.read()
         return book_text
 
-num_words = 0
-def word_count(book_text):
-    num_words = book_text.split()
-    print(f"{num_words} words found in the document")
-
-
 def main(file_path):
     book_text = get_book_text(file_path)
-    print(book_text)
+    num_words = len(book_text.split())
+    print(f"{num_words} words found in the document.")
 
 main("books/frankenstein.txt")
